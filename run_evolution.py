a33b1e0ccc5c3238c0b5e79dc631d64d828b9b0c
steven <steven.zg@outlook.com>
2025-04-22 08:09:04 +1200

    chore: update directory

--- run_evolution.py
+++ run_evolution.py
@@ -4,7 +4,7 @@ Entry point script for running AGIR Learning Evolution Process
 """
 
 import sys
-from agir_learning.cli import main
+from src.cli import main
 
 if __name__ == "__main__":
     sys.exit(main()) 
