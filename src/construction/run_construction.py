86352218c30af98b012441b86bee0926eabdf3e8
steven <steven.zg@outlook.com>
2025-05-16 18:03:49 +1200

    refactor: rename files

--- src/construction/run_construction.py
+++ src/construction/run_construction.py
@@ -1,10 +1,10 @@
 import logging
 
-from src.construction.create_or_find_learner import create_or_find_learner
-from src.construction.create_or_find_scenario import create_or_find_scenario
-from src.construction.create_or_find_agent_roles import create_or_find_agent_roles
-from src.construction.create_or_find_states import create_or_find_states
-from src.construction.create_or_find_state_transitions import create_or_find_state_transitions
+from src.construction.a_create_or_find_learner import create_or_find_learner
+from src.construction.b_create_or_find_scenario import create_or_find_scenario
+from src.construction.c_create_or_find_agent_roles import create_or_find_agent_roles
+from src.construction.d_create_or_find_states import create_or_find_states
+from src.construction.e_create_or_find_state_transitions import create_or_find_state_transitions
 from src.construction.check_database_tables import check_database_tables
 from typing import Dict, Any, List, Optional, Tuple, Union
 from src.utils.yaml_loader import load_scenario_from_file
@@ -38,27 +38,27 @@ def run_construction(yaml_file_path: str, created_by: Optional[str] = None) -> O
         try:
             db = next(get_db())
             
-            # 1. Create or find target user
+            # a. Create or find target user
             learner_id = create_or_find_learner(db, yaml_scenario.learner)
             if not learner_id:
                 return None
             
-            # 2. Create or find scenario
+            # b. Create or find scenario
             scenario_id = create_or_find_scenario(db, yaml_scenario.name, yaml_scenario.description, yaml_scenario.learner_role, created_by, learner_id)
             if not scenario_id:
                 return None
             
-            # 3. Create roles
+            # c. Create roles
             role_id_mapping = create_or_find_agent_roles(db, scenario_id, yaml_scenario.roles)
             if not role_id_mapping:
                 return None
             
-            # 4. Create states
+            # d. Create states
             state_id_mapping = create_or_find_states(db, scenario_id, yaml_scenario.states)
             if not state_id_mapping:
                 return None
             
-            # 5. Create transitions
+            # e. Create transitions
             if not create_or_find_state_transitions(db, scenario_id, yaml_scenario.transitions, state_id_mapping):
                 return None
             
