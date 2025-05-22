c11a4c2144bfff4d785a783d4f1d79d83c03fb4b
steven <steven.zg@outlook.com>
2025-05-23 07:43:05 +1200

    fix: navbar mx auto

--- web/src/components/layout/navbar.tsx
+++ web/src/components/layout/navbar.tsx
@@ -41,7 +41,7 @@ export function Navbar() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background">
-      <div className="container flex h-16 items-center justify-between">
+      <div className="container mx-auto flex h-16 items-center justify-between">
         <div className="flex items-center">
           <div className="mr-4 md:hidden">
             <Button
